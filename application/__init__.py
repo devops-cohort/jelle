ae4779f260e69b971666132cf45a48c2cf79e9de
jellevink <jelle.vinkenoog@academytrainee.com>
2019-12-05 10:15:07 +0000

    help

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI']=('mysql+pymysql://'+getenv('MYSQL_USER')+':'+getenv('MYSQL_PASSWORD')+'@'+getenv('MYSQL_HOST')+'/'+getenv('MYSQL_DB'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SECRET_KEY'] = (getenv('YOUR_SECRET_KEY'))
+db = SQLAlchemy(app)
 
 from application import routes

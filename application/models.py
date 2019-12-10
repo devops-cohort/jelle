d1ea477af32ee0c53f9dc9b2857cdd12ec6c0b5c
jellevink <57941484+jellevink@users.noreply.github.com>
2019-12-10 15:08:47 +0000

    Cleaning and commenting

--- application/models.py
+++ application/models.py
@@ -6,32 +6,30 @@ from sqlalchemy.orm import relationship
 def load_user(id):
     return User.query.get(int(id))
 
-
-"""substable = db.Table('subs',
-        db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-        db.Column('pokemon_id', db.Integer, db.ForeignKey('pokemon.pokemon_id'))
-        )"""
-
+#Creates user table
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
+    #sets relationship to the pokemon table
     pokemon = db.relationship('Pokemon', backref='user', lazy=True)
     
+    #Attributes of the class
     def __repr__(self):
         return ''.join([
             'User ID: ', str(self.username)
             ])
-
+#Creates pokemon table
 class Pokemon(db.Model):
     pokemonid = db.Column(db.Integer, primary_key=True)
     pokemon_name = db.Column(db.String(30), nullable=False, unique=True)
     pokemon_fast = db.Column(db.String(30), nullable=False, unique=False)
     pokemon_charge = db.Column(db.String(30), nullable=False, unique=True)
+    #sets link to user table
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
-
+    #Attributes of the class    
     def __repr__(self):
         return ''.join([
             'Pokemon Name: ', str(self.pokemon_name), '\r\n',
